ee3ef783b4b35511534321f9dd8e1f0e51fad8f6
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-04-18 13:41:36 +0530

    add

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,30 +1,43 @@
 const CategorySchema=require('../model/CategorySchema');
 const createCategory =async (request,response)=>{
-    const category=new CategorySchema({
-        categoryName:request.body.categoryName,
-        icon:{
-            hash:'Temp Hash',
-            resourceUrl:'',
-            fileName:'Temp FIle name ',
-            directory:''},
-        availableCountries:[
-            {
-                countryId:'Temp-Id-1',
-                countryName:'Sri lanka,'
-            },
-            {
-                countryId:'Temp-Id-2',
-                countryName:'USA,'
-            }
-        ],
 
-    });
-    category.save().then(result=>{
-        response.status(201)
+    try{
 
-    }).catch(err=>{
+        const { categoryName, file, countryIds } = request.body;
+        if(!categoryName || !file || !countryIds){
+            return response.status(400).json({code:400,message:'some field are missing!....',data:null})
+        }
 
-    })
+        const category=new CategorySchema({
+            categoryName:request.body.categoryName,
+            icon:{
+                hash:'Temp Hash',
+                resourceUrl:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dogster.com%2Fdog-breeds%2Fsiberian-husky&psig=AOvVaw3OsoJffBOD68J582Yo3wjO&ust=1745048997121000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJC4k6KM4YwDFQAAAAAdAAAAABAE',
+                fileName:'Temp FIle name ',
+                directory:''},
+            availableCountries:[
+                {
+                    countryId:'Temp-Id-1',
+                    countryName:'Sri lanka,'
+                },
+                {
+                    countryId:'Temp-Id-2',
+                    countryName:'USA,'
+                }
+            ],
+
+        });
+        const saveData=await category.save();
+        console.log(saveData);
+        return response.status(201).json({code:201,message:'customer has been saved..',data:saveData})
+
+    }catch (err){
+        response.status(500).json({ code: 500, message: 'Something went wrong...', error: err.message });
+    }
+
+
+
+
 
 }
 const updateCategory =async (request,response)=>{
